b0ef24197927eba2c5a3648947f3f37c6aea1bb8
ibarguen-dev <jt158161@gmail.com>
2023-11-26 10:09:43 -0500

    se realiza actualizaciones

--- chat.py
+++ chat.py
@@ -1,4 +1,11 @@
 from openai import OpenAI, AsyncOpenAI
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.setProperty('voice','''HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0''')
+
+# for voice in engine.getProperty('voice'):
+#     print(voice)
 
 client = OpenAI(
   api_key='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',  # this is also the default, it can be omitted
@@ -8,11 +15,12 @@ response_iterator = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "como craer un ia",
+            "content": "en una palabra define que es  AGI",
         }
     ],
     model="gpt-3.5-turbo",
     stream=True,
+    max_tokens=10
 )
 collected_messages = []
 
@@ -25,8 +33,9 @@ for chunk in response_iterator:
     full_reply_content = ''.join(cleaned_messages)
     print(full_reply_content)
 
-    # clear the terminal
+    #clear the terminal
     print("\033[H\033[J", end="")
 
 print(full_reply_content)
-
+engine.say(full_reply_content)
+engine.runAndWait()
